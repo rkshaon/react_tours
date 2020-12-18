9f0c80bebec7fdf73d89d92714a5ca03ba1b4138
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-12-18 16:35:05 +0600

    tours project done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
+import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch(error){
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  }, [])
+
   if(loading){
     return(
       <main>
@@ -13,9 +38,19 @@ function App() {
       </main>
     );
   }
+  if(tours.length === 0){
+    return(
+      <main>
+        <div className="title">
+          <h2>No tour left</h2>
+          <button className="btn" onClick={fetchTours}>refresh</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Loading />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
